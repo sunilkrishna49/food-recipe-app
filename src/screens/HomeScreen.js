a776d0b9cd325db93598c28c8db0058553785fec
sunilkrishna49 <sunilkrishna49@gmail.com>
2023-10-09 14:35:04 +0100

    home screen - completed avatar,bell-icon,greetings.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,51 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, Image } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import * as Icons from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
   return (
-    <View className="flex-1 flex-col items-center justify-center">
-      <Text className="text-4xl">Home Screen</Text>
+    <View className="flex-1 bg-white">
+      <StatusBar style="dark" />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 50 }}
+        className="space-y-6 pt-14"
+      >
+        {/* avatar and bell icon */}
+        <View className="mx-4 flex-row justify-between items-center mb-2">
+          <Image
+            source={require("../../assets/images/avatar.png")}
+            style={{ height: hp(5), width: hp(5.5) }}
+          />
+          <Icons.BellIcon size={hp(4)} color="gray" />
+        </View>
+
+        {/* greetings and punchline */}
+        <View className="mx-4 space-y-2 mb-2">
+          <Text style={{ fontSize: hp(1.7) }} className="text-neutral-600">
+            Hello, Tansi!
+          </Text>
+          <View>
+            <Text
+              style={{ fontSize: hp(3.8) }}
+              className="text-neutral-600 font-semibold"
+            >
+              Make your own food,{" "}
+            </Text>
+          </View>
+          <Text
+            style={{ fontSize: hp(3.8) }}
+            className="font-semibold text-neutral-600"
+          >
+            stay at <Text className="text-amber-400">home</Text>
+          </Text>
+        </View>
+      </ScrollView>
     </View>
   );
 };
