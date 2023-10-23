2b693b14b85f35a6fe67ad4d76b2452773e64b2c
sunilkrishna49 <sunilkrishna49@gmail.com>
2023-10-24 00:14:49 +0100

    RecipeDetailsScreen - clock icons done

--- src/screens/RecipeDetailsScreen.js
+++ src/screens/RecipeDetailsScreen.js
@@ -1,18 +1,49 @@
 import { View, Text, Image, TouchableOpacity, ScrollView } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import { StatusBar } from "expo-status-bar";
-import { ChevronLeftIcon } from "react-native-heroicons/outline";
+import {
+  ChevronLeftIcon,
+  ClockIcon,
+  FireIcon,
+  Square3Stack3DIcon,
+  Squares2X2Icon,
+  UsersIcon,
+} from "react-native-heroicons/outline";
 import { HeartIcon } from "react-native-heroicons/solid";
 import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+import Loading from "../components/Loading";
 
 export default function RecipeDetailsScreen(props) {
+  let item = props.route.params;
   const navigation = useNavigation();
   const [isFavourite, setIsFavourite] = useState(false);
-  let item = props.route.params;
+  const [meal, setMeal] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getMealDetails(item.idMeal);
+  }, []);
+
+  const getMealDetails = async (id) => {
+    try {
+      const response = await axios.get(
+        `https://themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+      );
+      // console.log("got meal details: ", response.data);
+      if (response && response.data) {
+        setMeal(response.data.meals[0]);
+        setLoading(false);
+      }
+    } catch (err) {
+      console.log("error: ", err.message);
+    }
+  };
+
   return (
     <ScrollView
       className="bg-white flex-1"
@@ -46,6 +77,122 @@ export default function RecipeDetailsScreen(props) {
           />
         </TouchableOpacity>
       </View>
+      {/* meal description */}
+      {loading ? (
+        <Loading size="large" className="mt-16" />
+      ) : (
+        <View className="px-4 flex justify-between space-y-4 pt-8">
+          {/* name and area */}
+          <View className="space-y-2">
+            <Text
+              style={{ fontSize: hp(3) }}
+              className="flex-1 font-bold text-neutral-700"
+            >
+              {meal?.strMeal}
+            </Text>
+
+            <Text
+              style={{ fontSize: hp(2) }}
+              className="flex-1 font-medium text-neutral-500"
+            >
+              {meal?.strArea}
+            </Text>
+          </View>
+          {/* misc */}
+          <View className="flex-row justify-around">
+            <View className="flex rounded-full bg-amber-300 p-2">
+              <View
+                style={{ height: hp(6.5), width: hp(6.5) }}
+                className="bg-white rounded-full flex items-center justify-center"
+              >
+                <ClockIcon size={hp(4)} strokeWidth={2.5} color="#525252" />
+              </View>
+              <View className="flex items-center py-2 space-y-1">
+                <Text
+                  style={{ fontSize: hp(2) }}
+                  className="font-bold text-neutral-700"
+                >
+                  35
+                </Text>
+                <Text
+                  style={{ fontSize: hp(2) }}
+                  className="font-bold text-neutral-700"
+                >
+                  Mins
+                </Text>
+              </View>
+            </View>
+            <View className="flex rounded-full bg-amber-300 p-2">
+              <View
+                style={{ height: hp(6.5), width: hp(6.5) }}
+                className="bg-white rounded-full flex items-center justify-center"
+              >
+                <UsersIcon size={hp(4)} strokeWidth={2.5} color="#525252" />
+              </View>
+              <View className="flex items-center py-2 space-y-1">
+                <Text
+                  style={{ fontSize: hp(2) }}
+                  className="font-bold text-neutral-700"
+                >
+                  03
+                </Text>
+                <Text
+                  style={{ fontSize: hp(2) }}
+                  className="font-bold text-neutral-700"
+                >
+                  Servings
+                </Text>
+              </View>
+            </View>
+            <View className="flex rounded-full bg-amber-300 p-2">
+              <View
+                style={{ height: hp(6.5), width: hp(6.5) }}
+                className="bg-white rounded-full flex items-center justify-center"
+              >
+                <FireIcon size={hp(4)} strokeWidth={2.5} color="#525252" />
+              </View>
+              <View className="flex items-center py-2 space-y-1">
+                <Text
+                  style={{ fontSize: hp(2) }}
+                  className="font-bold text-neutral-700"
+                >
+                  103
+                </Text>
+                <Text
+                  style={{ fontSize: hp(2) }}
+                  className="font-bold text-neutral-700"
+                >
+                  Cal
+                </Text>
+              </View>
+            </View>
+            <View className="flex rounded-full bg-amber-300 p-2">
+              <View
+                style={{ height: hp(6.5), width: hp(6.5) }}
+                className="bg-white rounded-full flex items-center justify-center"
+              >
+                <Square3Stack3DIcon
+                  size={hp(4)}
+                  strokeWidth={2.5}
+                  color="#525252"
+                />
+              </View>
+              <View className="flex items-center py-2 space-y-1">
+                <Text
+                  style={{ fontSize: hp(2) }}
+                  className="font-bold text-neutral-700"
+                ></Text>
+                <Text
+                  style={{ fontSize: hp(2) }}
+                  className="font-bold text-neutral-700"
+                >
+                  Easy
+                </Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      )}
     </ScrollView>
   );
 }
